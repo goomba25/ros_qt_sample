07e5509a41b4927deb91d7e3334cb22dc34c277a
Felix <ajtwls0205@naver.com>
2021-07-23 11:48:21 +0900

    add file

--- qt_ros_test-build/CMakeFiles/qt_ros_test_node_automoc.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "qt_ros_test_node_automoc.cpp"
-  "CMakeFiles/qt_ros_test_node_automoc"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/qt_ros_test_node_automoc.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- qt_ros_test-build/atomic_configure/local_setup.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-# generated from catkin/cmake/template/local_setup.sh.in
-
-# since this file is sourced either use the provided _CATKIN_SETUP_DIR
-# or fall back to the destination set at configure time
-: ${_CATKIN_SETUP_DIR:=/home/hills/catkin_ws/src/ROS_QT_GUI/qt_ros_test-build/devel}
-CATKIN_SETUP_UTIL_ARGS="--extend --local"
-. "$_CATKIN_SETUP_DIR/setup.sh"
-unset CATKIN_SETUP_UTIL_ARGS
